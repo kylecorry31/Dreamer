1a8beda646f15f08fd3586ee70f455875e9c2fb3
Kyle Corry <kylecorry31@gmail.com>
2022-08-26 13:53:28 -0400

    Update AppDatabase.kt

--- app/src/main/java/com/kylecorry/oneironaut/app/AppDatabase.kt
+++ app/src/main/java/com/kylecorry/oneironaut/app/AppDatabase.kt
@@ -35,7 +35,7 @@ abstract class AppDatabase : RoomDatabase() {
 
         private fun buildDatabase(context: Context): AppDatabase {
             return Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME)
-//                .addMigrations()
+                .addMigrations()
                 .build()
         }
     }
